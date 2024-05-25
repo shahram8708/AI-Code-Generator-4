dc9d1d1a9eabb77d981f4f7bca6c96dfecd80a31
Shah Ram <152763547+shahram8708@users.noreply.github.com>
2024-05-25 09:27:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -23,27 +23,33 @@ def generate_code_from_github(requirements):
 
     headers = {
         'Authorization': f'token {GITHUB_TOKEN}',
-        'Accept': 'application/vnd.github.v3.raw'
+        'Accept': 'application/vnd.github.v3+json'
     }
+    
+    file_extensions = [
+        'html', 'css','py', 'java', 'cpp', 'c', 'cs', 'js', 'ts', 'php', 'rb', 'go', 'pl', 'scala', 'groovy', 'r', 'swift', 'kotlin', 'rust', 'lua', 'm', 'asm', 'h', 'hpp', 's', 'vhdl', 'f', 'f90', 'f95', 'pas', 'lisp', 'ml', 'hs', 'erl', 'dart', 'vala', 'd', 'rkt', 'julia', 'nim', 'tcl', 'awk', 'fortran', 'ada', 'ocaml', 'matlab'
+    ]
+    query = f'{requirements} in:file ' + ' '.join([f'extension:{ext}' for ext in file_extensions])
+    
     params = {
-        'q': f'{requirements} in:file',
+        'q': query,
         'per_page': 5
     }
+
     try:
         response = requests.get('https://api.github.com/search/code', headers=headers, params=params)
-        response.raise_for_status() 
+        response.raise_for_status()
         results = response.json()
         for item in results.get('items', []):
             repository = item.get('repository', {}).get('full_name')
             path = item.get('path')
             url = f'https://raw.githubusercontent.com/{repository}/master/{path}'
-            code_response = requests.get(url, headers=headers)
+            code_response = requests.get(url, headers={'Authorization': f'token {GITHUB_TOKEN}'})
             if code_response.status_code == 200:
                 return code_response.text
-        return "No code generate found for the given requirements. Please try again later. 🙏🙏🙏"
+        return "No suitable code found for the given requirements. Please try again later. 🙏🙏🙏"
     except requests.RequestException as e:
-        return f"Error generate code: {e} Please try again later. 🙏🙏🙏"
-
+        return f"Please try again later. 🙏🙏🙏"
 
 if __name__ == '__main__':
     app.run(debug=True)
